ec959e13c35163195f84e5a1ab01429a9c04af92
Grrrdolf <herseyb71@gmail.com>
2020-12-05 14:42:48 -0800

    Added more code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 const slotSymbols = document.querySelectorAll('.slot-symbols-content h2');
-
+const scoreNumber = document.querySelector('.score-number');
 
 function rollImages(){
     let slotInnerContent = document.querySelectorAll('.slot-symbols-content');
-    let random = Math.floor(Math.random(4) * 5);
+
+    let random = Math.floor(Math.random(5) * 5);
     console.log(slotSymbols[random]);
 
-    let random1 = Math.floor(Math.random(4) * 5);
-    console.log(slotSymbols[random]);
+    let random1 = Math.floor(Math.random(5) * 5);
+    console.log(slotSymbols[random1]);
 
-    let random2 = Math.floor(Math.random(4) * 5);
-    console.log(slotSymbols[random]);
+    let random2 = Math.floor(Math.random(5) * 5);
+    console.log(slotSymbols[random2]);
 
     slotInnerContent[0].innerHTML = `<h2>${slotSymbols[random].textContent}</h2>`;
 
@@ -18,4 +19,4 @@ function rollImages(){
 
     slotInnerContent[2].innerHTML = `<h2>${slotSymbols[random2].textContent}</h2>`;
 
-}
+};
